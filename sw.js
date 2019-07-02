5ac9c17072915353b8c1daa255f10c13b8702b82
thehardestpart <thehardestpart@protonmail.com>
2019-07-02 20:58:22 +0200

    Updated cache.

--- sw.js
+++ sw.js
@@ -1,25 +1,40 @@
-var cacheName = 'hello-pwa';
+// https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+var cacheName = 'coin-flip-v0';
 var filesToCache = [
   '/',
   '/index.html',
   '/css/style.css',
-  '/js/main.js'
+  '/js/main.js',
+  '/images/favicon.ico',
+  '/images/favicon-48.png',
+  '/images/favicon-72.png',
+  '/images/favicon-96.png',
+  '/images/favicon-144.png',
+  '/images/favicon-192.png',
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+	e.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			console.log('[Service Worker] Caching files');
+			return cache.addAll(filesToCache);
+		})
+	);
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+	e.respondWith(
+		caches.match(e.request).then(function(r) {
+			console.log('[Service Worker] Fetching resource: '+e.request.url);
+			return r || fetch(e.request).then(function(response) {
+				return caches.open(cacheName).then(function(cache) {
+					console.log('[Service Worker] Caching new resource: '+e.request.url);
+					cache.put(e.request, response.clone());
+					return response;
+				});
+			});
+		})
+	);
 });
